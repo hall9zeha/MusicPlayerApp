455c3b4eadd61afdf36780a2e0678f3e19c7211c
hall9zeha <vmwaretars@gmail.com>
2024-04-23 12:48:44 -0500

    Added fragments and move main design to main fragment

--- app/src/main/java/com/barryzeha/ktmusicplayer/view/activities/MainActivity.kt
+++ app/src/main/java/com/barryzeha/ktmusicplayer/view/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.barryzeha.ktmusicplayer
+package com.barryzeha.ktmusicplayer.view.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.barryzeha.ktmusicplayer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
