c418ddb68bab781d5ff6da250a09c460f16d0636
hall9zeha <vmwaretars@gmail.com>
2024-04-26 11:04:43 -0500

    Added Adapter for music files

---
+++ app/src/main/java/com/barryzeha/ktmusicplayer/view/adapters/MusicListAdapter.kt
@@ -0,0 +1,11 @@
+package com.barryzeha.ktmusicplayer.view.adapters
+
+
+/**
+ * Project KTMusicPlayer
+ * Created by Barry Zea H. on 26/4/24.
+ * Copyright (c)  All rights reserved.
+ **/
+
+class MusicListAdapter {
+}
