c4fd697f6cccfabc3e32c12f415f921ffff90705
hall9zeha <vmwaretars@gmail.com>
2024-04-27 13:09:41 -0500

    chore list player fragment design

--- app/src/main/java/com/barryzeha/ktmusicplayer/view/fragments/ListPlayerFragment.kt
+++ app/src/main/java/com/barryzeha/ktmusicplayer/view/fragments/ListPlayerFragment.kt
@@ -6,21 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.barryzeha.ktmusicplayer.R
+import com.barryzeha.ktmusicplayer.databinding.FragmentListPlayerBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ListPlayerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ListPlayerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
+    private var _bind:FragmentListPlayerBinding? = null
+    private val bind:FragmentListPlayerBinding get() = _bind!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,20 +30,19 @@ class ListPlayerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_player, container, false)
+        activity?.let{
+            _bind = FragmentListPlayerBinding.inflate(inflater,container,false)
+            _bind?.let { bind-> return bind.root }
+        }
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListPlayerFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ListPlayerFragment().apply {
