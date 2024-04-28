5e22896b5ceae3e33889cd47f327c96d35184b57
hall9zeha <vmwaretars@gmail.com>
2024-04-28 09:46:31 -0500

    Added constants file

---
+++ app/src/main/java/com/barryzeha/ktmusicplayer/common/Constants.kt
@@ -0,0 +1,11 @@
+package com.barryzeha.ktmusicplayer.common
+
+
+/**
+ * Project KTMusicPlayer
+ * Created by Barry Zea H. on 28/4/24.
+ * Copyright (c)  All rights reserved.
+ **/
+
+const val MAIN_FRAGMENT = 0
+const val SONG_LIST_FRAGMENT = 1
--- app/src/main/java/com/barryzeha/ktmusicplayer/view/adapters/PageCollectionAdapter.kt
+++ app/src/main/java/com/barryzeha/ktmusicplayer/view/adapters/PageCollectionAdapter.kt
@@ -3,6 +3,8 @@ package com.barryzeha.ktmusicplayer.view.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.barryzeha.ktmusicplayer.common.MAIN_FRAGMENT
+import com.barryzeha.ktmusicplayer.common.SONG_LIST_FRAGMENT
 import com.barryzeha.ktmusicplayer.view.fragments.ListPlayerFragment
 import com.barryzeha.ktmusicplayer.view.fragments.MainPlayerFragment
 
@@ -18,8 +20,8 @@ class PageCollectionAdapter(fragment:FragmentActivity, private val listOfTitles:
 
     override fun createFragment(position: Int): Fragment {
         return when(position){
-            0->MainPlayerFragment()
-            1->ListPlayerFragment()
+            MAIN_FRAGMENT->MainPlayerFragment()
+            SONG_LIST_FRAGMENT->ListPlayerFragment()
             else->MainPlayerFragment()
         }
 
