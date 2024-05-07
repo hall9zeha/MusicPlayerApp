32f652a4d1aa487511656e789257f0c2ad73d439
hall9zeha <vmwaretars@gmail.com>
2024-05-07 11:51:06 -0500

    Refactor

--- app/src/main/java/com/barryzeha/ktmusicplayer/view/ui/fragments/ListPlayerFragment.kt
+++ app/src/main/java/com/barryzeha/ktmusicplayer/view/ui/fragments/ListPlayerFragment.kt
@@ -23,6 +23,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.barryzeha.core.common.checkPermissions
+import com.barryzeha.core.common.getRealPathFromURI
 import com.barryzeha.core.common.showSnackBar
 import com.barryzeha.core.entities.SongEntity
 import com.barryzeha.ktmusicplayer.databinding.FragmentListPlayerBinding
@@ -87,9 +88,9 @@ class ListPlayerFragment : Fragment() {
         launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result:ActivityResult->
             if(result.resultCode == Activity.RESULT_OK){
                 uri = result.data?.data
-                val probe = getRealPathFromURI(uri!!, requireContext())
+                val realPathFromFile = getRealPathFromURI(uri!!, requireContext())
                 mainViewModel.saveNewSong(SongEntity(
-                    pathLocation = probe,
+                    pathLocation = realPathFromFile,
                     timestamp = Date().time
                 ))
 
@@ -181,40 +182,7 @@ class ListPlayerFragment : Fragment() {
             }*/
         }
     }
-    fun getRealPathFromURI(uri: Uri, context: Context): String? {
-        val returnCursor = context.contentResolver.query(uri, null, null, null, null)
-        val nameIndex =  returnCursor!!.getColumnIndex(OpenableColumns.DISPLAY_NAME)
-        val sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE)
-        returnCursor.moveToFirst()
-        val name = returnCursor.getString(nameIndex)
-        val size = returnCursor.getLong(sizeIndex).toString()
-        val file = File(context.filesDir, name)
-        try {
-            val inputStream: InputStream? = context.contentResolver.openInputStream(uri)
-            val outputStream = FileOutputStream(file)
-            var read = 0
-            val maxBufferSize = 1 * 1024 * 1024
-            val bytesAvailable: Int = inputStream?.available() ?: 0
-            //int bufferSize = 1024;
-            val bufferSize = Math.min(bytesAvailable, maxBufferSize)
-            val buffers = ByteArray(bufferSize)
-            while (inputStream?.read(buffers).also {
-                    if (it != null) {
-                        read = it
-                    }
-                } != -1) {
-                outputStream.write(buffers, 0, read)
-            }
-            Log.e("File Size", "Size " + file.length())
-            inputStream?.close()
-            outputStream.close()
-            Log.e("File Path", "Path " + file.path)
 
-        } catch (e: java.lang.Exception) {
-            Log.e("Exception", e.message!!)
-        }
-        return file.path
-    }
 
     companion object {
 
--- core/src/main/java/com/barryzeha/core/common/Helpers.kt
+++ core/src/main/java/com/barryzeha/core/common/Helpers.kt
@@ -2,7 +2,13 @@ package com.barryzeha.core.common
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.OpenableColumns
+import android.util.Log
 import androidx.core.content.ContextCompat
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 
@@ -23,8 +29,42 @@ fun checkPermissions(context: Context, permission:String, isGranted:(Boolean) ->
 }
 fun getTimeOfSong(duration:Long):String{
     return String.format(
-        Locale.ROOT,"%02d:%02d",
+        Locale.ROOT,"::%02d:%02d",
         TimeUnit.MILLISECONDS.toMinutes(duration.toLong()),
         TimeUnit.MILLISECONDS.toSeconds(duration.toLong()) -
         TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration.toLong())))
+}
+fun getRealPathFromURI(uri: Uri, context: Context): String? {
+    val returnCursor = context.contentResolver.query(uri, null, null, null, null)
+    val nameIndex =  returnCursor!!.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+    val sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE)
+    returnCursor.moveToFirst()
+    val name = returnCursor.getString(nameIndex)
+    val size = returnCursor.getLong(sizeIndex).toString()
+    val file = File(context.filesDir, name)
+    try {
+        val inputStream: InputStream? = context.contentResolver.openInputStream(uri)
+        val outputStream = FileOutputStream(file)
+        var read = 0
+        val maxBufferSize = 1 * 1024 * 1024
+        val bytesAvailable: Int = inputStream?.available() ?: 0
+        //int bufferSize = 1024;
+        val bufferSize = Math.min(bytesAvailable, maxBufferSize)
+        val buffers = ByteArray(bufferSize)
+        while (inputStream?.read(buffers).also {
+                if (it != null) {
+                    read = it
+                }
+            } != -1) {
+            outputStream.write(buffers, 0, read)
+        }
+        Log.e("File Size", "Size " + file.length())
+        inputStream?.close()
+        outputStream.close()
+        Log.e("File Path", "Path " + file.path)
+
+    } catch (e: java.lang.Exception) {
+        Log.e("Exception", e.message!!)
+    }
+    return file.path
 }
