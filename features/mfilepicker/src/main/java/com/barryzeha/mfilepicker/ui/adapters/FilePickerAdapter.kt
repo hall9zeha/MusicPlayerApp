a0f72e5d5568dacd9f6cba43ac0831009b6d6ca8
hall9zeha <vmwaretars@gmail.com>
2024-07-31 10:51:47 -0500

    Added file picker item

---
+++ features/mfilepicker/src/main/java/com/barryzeha/mfilepicker/ui/adapters/FilePickerAdapter.kt
@@ -0,0 +1,11 @@
+package com.barryzeha.mfilepicker.ui.adapters
+
+
+/**
+ * Project KTMusicPlayer
+ * Created by Barry Zea H. on 31/7/24.
+ * Copyright (c)  All rights reserved.
+ **/
+
+class FilePickerAdapter {
+}
