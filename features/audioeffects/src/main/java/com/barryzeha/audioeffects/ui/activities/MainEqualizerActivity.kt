7b0809af4f56887a83acf70c2eed1bdbfc37451c
hall9zeha <vmwaretars@gmail.com>
2024-08-29 12:27:40 -0500

    Added audioeffects module

---
+++ features/audioeffects/src/main/java/com/barryzeha/audioeffects/ui/activities/MainEqualizerActivity.kt
@@ -0,0 +1,21 @@
+package com.barryzeha.audioeffects.ui.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.barryzeha.audioeffects.R
+
+class MainEqualizerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_equalizer)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
